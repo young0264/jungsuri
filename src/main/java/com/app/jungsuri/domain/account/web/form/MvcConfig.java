16f4d8ebe2cb2130ced7ed16632fba23b6126ddb
남의영 <ny2485@naver.com>
2023-04-30 23:00:57 +0900

    spring security settings

--- src/main/java/com/app/jungsuri/domain/account/web/form/MvcConfig.java
+++ src/main/java/com/app/jungsuri/domain/account/web/form/MvcConfig.java
@@ -7,13 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/main").setViewName("main");
-    }
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/home").setViewName("home");
+//        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/hello").setViewName("hello");
+//        registry.addViewController("/login").setViewName("login");
+//    }
 
 
 }
--- src/main/java/com/app/jungsuri/domain/account/web/form/SignUpForm.java
+++ src/main/java/com/app/jungsuri/domain/account/web/form/SignUpForm.java
@@ -1,2 +1,41 @@
-package com.app.jungsuri.domain.account.web.form;public class SignUpForm {
+package com.app.jungsuri.domain.account.web.form;
+
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpForm {
+
+    @NotBlank
+//    @Length(min=3, max=20)
+//    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
+    private String loginId;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+//    @Length(min=8 , max=50)
+    private String password;
+
+    private String name;
+
+    private String bio;
+
+    private String location;
+
+    @Lob
+    private String profileImage;
+
 }
