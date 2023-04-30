412fbc9eefb922e85b9353f1591c6ee85e813052
남의영 <ny2485@naver.com>
2023-04-30 10:15:42 +0900

    Initial commit

---
+++ src/main/java/com/app/jungsuri/JungsuriApplication.java
@@ -0,0 +1,15 @@
+package com.app.jungsuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungsuriApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JungsuriApplication.class, args);
+	}
+
+
+}
