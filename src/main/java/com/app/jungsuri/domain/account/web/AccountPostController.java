2d8375b6e3f458432ca09c3e64b5fa99e41e7f83
남의영 <ny2485@naver.com>
2023-04-30 23:01:21 +0900

    PostConstruct initial setting

--- src/main/java/com/app/jungsuri/domain/account/web/AccountPostController.java
+++ src/main/java/com/app/jungsuri/domain/account/web/AccountPostController.java
@@ -1,2 +1,26 @@
-package com.app.jungsuri.domain.account.web;public class AccountPostController {
+package com.app.jungsuri.domain.account.web;
+
+import com.app.jungsuri.domain.account.persistence.AccountService;
+import com.app.jungsuri.domain.account.web.form.SignUpForm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * initial data 초기화
+ */
+
+@Controller
+public class AccountPostController {
+
+    @Autowired
+    private AccountService accountService;
+
+
+    @PostConstruct
+    public String init() {
+        SignUpForm signUpForm = new SignUpForm("12", "12@naver.com", "12",",","","","");
+        accountService.createNewAccount(signUpForm);
+        return "main";
+    }
 }
