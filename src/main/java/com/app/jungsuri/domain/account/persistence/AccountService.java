eb8d527c8f9bde973662adf7e7a70c8f58a8ce2f
남의영 <ny2485@naver.com>
2023-05-01 14:22:33 +0900

    add : security settings

--- src/main/java/com/app/jungsuri/domain/account/persistence/AccountService.java
+++ src/main/java/com/app/jungsuri/domain/account/persistence/AccountService.java
@@ -1,14 +1,11 @@
 package com.app.jungsuri.domain.account.persistence;
 
 import com.app.jungsuri.domain.account.web.form.SignUpForm;
-import jakarta.annotation.PostConstruct;
+import com.app.jungsuri.domain.account.web.form.UserAccount;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration;
-import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
@@ -26,45 +23,45 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class AccountService implements UserDetailsService {
     private final AccountRepository accountRepository;
-//    private final ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
 
-    ModelMapper modelMapper = new ModelMapper();
 
 
     public void login(AccountEntity accountEntity) {
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-                accountEntity,
+        System.out.println("로그인 시작");
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+//                new UserAccount(accountEntity),
+                accountEntity.getLoginId(),
                 accountEntity.getPassword(),
                 List.of(new SimpleGrantedAuthority("ROLE_USER")));
+//        Authentication authentication = this.authenticationProvider.authenticate(token);
 
+//        SecurityContext context = SecurityContextHolder.getContext();
         SecurityContext context = SecurityContextHolder.createEmptyContext();
-        context.setAuthentication(authentication);
+        context.setAuthentication(token);
     }
 
     public AccountEntity createNewAccount(SignUpForm signUpForm) {
-        System.out.println(signUpForm.getEmail());
         signUpForm.setPassword(passwordEncodeByRawPassword(signUpForm.getPassword()));
-        initModelMapperSetting();
-        return accountRepository.save(modelMapper.map(signUpForm, AccountEntity.class));
+        AccountEntity accountEntity = accountRepository.save(modelMapper.map(signUpForm, AccountEntity.class));
+        return accountEntity;
     }
 
     private String passwordEncodeByRawPassword(String rawPassword) {
         return passwordEncoder.encode(rawPassword);
     }
 
-    private void initModelMapperSetting() {
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT)
-                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
-                .setFieldMatchingEnabled(true);
-    }
+
 
     /**
      * spring security
      */
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
         AccountEntity accountEntity = accountRepository.findByLoginId(loginId);
@@ -79,7 +76,8 @@ public class AccountService implements UserDetailsService {
         //AccountContext 생성자로 UserDetails 타입 생성
         AccountContext accountContext = new AccountContext(accountEntity, roles);
 
-        return accountContext;
+//        return accountContext;
+        return new UserAccount(accountEntity);
     }
 
 }
--- src/main/java/com/app/jungsuri/domain/account/web/form/WebSecurityConfig.java
+++ src/main/java/com/app/jungsuri/domain/account/web/form/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.app.jungsuri.domain.account.web.form;
 
 import com.app.jungsuri.domain.account.persistence.CustomAuthenticationProvider;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -18,35 +17,41 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@RequiredArgsConstructor
 public class WebSecurityConfig implements WebMvcConfigurer {
 
-
     @Bean
     public AuthenticationProvider authenticationProvider() {
         return new CustomAuthenticationProvider();
     }
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/signup").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+                        .requestMatchers("/", "/home", "/signup", "/login").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+        http
                 .formLogin((form) -> form
                         .loginPage("/login")
                         .permitAll()
-                        .defaultSuccessUrl("/main", true))
-                .logout((logout) -> logout.permitAll());
+                        .defaultSuccessUrl("/", true));
+        http
+                .logout((logout) -> logout
+//                        .logoutSuccessUrl("/")
+                        .permitAll());
 
         return http.build();
     }
 
+
+    // PasswordEncoder가 bean으로만 등록되어 잇으면 spring security에서 사용함
+    // PasswordEncoder를 직접 생성하는게 아닌 createDelegatingPasswordEncoder에 위임
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    //static 파일 경로 접근
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web
