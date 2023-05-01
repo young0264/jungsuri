e41fde4e0cf54c066214dd26c9884c88b9744d43
남의영 <ny2485@naver.com>
2023-05-01 14:23:30 +0900

    AppConfig 생성.  - modelMapper 셋팅

--- src/main/java/com/app/jungsuri/infra/config/AppConfig.java
+++ src/main/java/com/app/jungsuri/infra/config/AppConfig.java
@@ -1,2 +1,22 @@
-package com.app.jungsuri.infra.config;public class AppConfig {
+package com.app.jungsuri.infra.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+//                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE)
+//                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE)
+                .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
 }
