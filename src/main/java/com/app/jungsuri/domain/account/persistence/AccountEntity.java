dbf604721a0068d5ca0bbd3dc59cdd8296b84ae8
남의영 <ny2485@naver.com>
2023-05-04 17:07:17 +0900

    account도메인, test code 코드정리

--- src/main/java/com/app/jungsuri/domain/account/persistence/AccountEntity.java
+++ src/main/java/com/app/jungsuri/domain/account/persistence/AccountEntity.java
@@ -2,13 +2,16 @@ package com.app.jungsuri.domain.account.persistence;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
 @Builder
+@Slf4j
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "account")
@@ -27,12 +30,12 @@ public class AccountEntity {
 
     @Column(unique = true)
     private String email;
-//
-//    private String emailToken;
-//
-//    private LocalDateTime emailCheckTokenGeneratedAt;
-//
-//    private boolean emailVerified;
+
+    private String emailToken;
+
+    private LocalDateTime emailCheckTokenGeneratedAt;
+
+    private boolean emailVerified;
 
     private String location; //TODO 대략적인 시 or 구까지.
 
@@ -46,4 +49,22 @@ public class AccountEntity {
     private String profileImage;
 
     private LocalDateTime joinedAt;
+
+    public boolean isValidToken(String token) {
+        return this.emailToken.equals(token);
+    }
+
+    public void completeSignUp() {
+        this.emailVerified = true;
+        joinedAt = LocalDateTime.now();
+    }
+
+    public void generateEmailCheckToken() {
+        this.emailToken = UUID.randomUUID().toString();
+        this.emailCheckTokenGeneratedAt = LocalDateTime.now();
+    }
+
+    public boolean canSendConfirmEmail() {
+        return this.emailCheckTokenGeneratedAt.isBefore(LocalDateTime.now().minusHours(1)); //1시간 이전에 만들었는지
+    }
 }
