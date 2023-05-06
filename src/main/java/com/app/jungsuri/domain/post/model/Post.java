0b5368dd7c889dc508983f1f0cdef6bfc1920f77
남의영 <ny2485@naver.com>
2023-05-07 04:21:09 +0900

    post domain CRUD 작업

--- src/main/java/com/app/jungsuri/domain/post/model/Post.java
+++ src/main/java/com/app/jungsuri/domain/post/model/Post.java
@@ -1,2 +1,37 @@
-package com.app.jungsuri.domain.post.model;public class Post {
+package com.app.jungsuri.domain.post.model;
+
+import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.infra.config.AppConfig.*;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+public class Post {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String imagePath;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+    private final String loginId;
+
+
+    public PostEntity toEntity(){
+        return PostEntity.builder()
+                .id(id)
+                .title(title)
+                .content(content)
+                .imagePath(imagePath)
+                .createdAt(createdAt)
+                .updatedAt(updatedAt)
+                .author(loginId)
+                .build();
+    }
+
+
+
 }
