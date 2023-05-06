5843210c74ee6a2ec448a25c03607e29b3f95e66
남의영 <ny2485@naver.com>
2023-05-06 23:34:28 +0900

    refactor : script단에서 summernote html태그 제거

---
+++ src/main/java/com/app/jungsuri/domain/post/web/dto/PostCreateDto.java
@@ -0,0 +1,13 @@
+package com.app.jungsuri.domain.post.web.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRegistDto {
+    String title;
+    String content;
+
+}
