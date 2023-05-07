42d885e966e126b4ae15aab69a50c530629ab24d
남의영 <ny2485@naver.com>
2023-05-07 17:46:11 +0900

    comment 모델 추가

--- src/main/java/com/app/jungsuri/domain/comment/persistence/CommentEntity.java
+++ src/main/java/com/app/jungsuri/domain/comment/persistence/CommentEntity.java
@@ -1,2 +1,42 @@
-package com.app.jungsuri.domain.comment.persistence;public class CommentEntity {
+package com.app.jungsuri.domain.comment.persistence;
+
+
+import com.app.jungsuri.domain.account.persistence.AccountEntity;
+import com.app.jungsuri.domain.post.persistence.PostEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@Table(name="comment")
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private String author;
+
+    private Long likeCount;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    private PostEntity postEntity;
+
+    @ManyToOne
+    private AccountEntity accountEntity;
+
 }
