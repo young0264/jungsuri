73e50d2286ecac85b057f1fc43cf2211094378b9
남의영 <ny2485@naver.com>
2023-05-14 21:50:17 +0900

    profile settings 비밀번호 변경 구현

--- src/main/java/com/app/jungsuri/domain/account/persistence/SettingsService.java
+++ src/main/java/com/app/jungsuri/domain/account/persistence/SettingsService.java
@@ -1,2 +1,44 @@
-package com.app.jungsuri.domain.account.persistence;public class SettingsService {
+package com.app.jungsuri.domain.account.persistence;
+
+import com.app.jungsuri.domain.account.web.dto.PasswordUpdateDto;
+import com.app.jungsuri.domain.post.persistence.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SettingsService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
+
+    public void updatePassword(PasswordUpdateDto passwordUpdateDto) {
+        AccountEntity accountEntity = accountRepository.findByLoginId(passwordUpdateDto.getLoginId()).orElseThrow(() -> new IllegalArgumentException("해당하는 아이디가 없습니다."));
+        isMatchPassword(passwordUpdateDto, accountEntity);
+        accountEntity.updatePassword(passwordEncoder.encode(passwordUpdateDto.getNewPassword()));
+    }
+
+    private void isMatchPassword(PasswordUpdateDto passwordUpdateDto, AccountEntity accountEntity) {
+        isMatchCurrentPassword(passwordUpdateDto, accountEntity);
+        isMatchNewPassword(passwordUpdateDto);
+    }
+
+    private void isMatchCurrentPassword(PasswordUpdateDto passwordUpdateDto, AccountEntity accountEntity) {
+        if (!passwordEncoder.matches(passwordUpdateDto.getCurrentPassword(), accountEntity.getPassword())) {
+            log.info("isMatchNewPassword :현재 비밀번호가 일치하지 않습니다. ");
+            throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    private void isMatchNewPassword(PasswordUpdateDto passwordUpdateDto) {
+        if (!passwordUpdateDto.getNewPassword().equals(passwordUpdateDto.getNewPasswordConfirm())) {
+            log.info("isMatchNewPassword :새로운 비밀번호가 일치하지 않습니다. ");
+            throw new IllegalArgumentException("새로운 비밀번호가 서로 일치하지 않습니다.");
+        }
+    }
 }
--- src/main/java/com/app/jungsuri/domain/account/web/dto/PasswordUpdateDto.java
+++ src/main/java/com/app/jungsuri/domain/account/web/dto/PasswordUpdateDto.java
@@ -1,2 +1,37 @@
-package com.app.jungsuri.domain.account.web.dto;public class passwordUpdateDto {
+package com.app.jungsuri.domain.account.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordUpdateDto {
+
+
+    @NotBlank
+    String loginId;
+
+    @NotBlank
+    String currentPassword;
+
+    @NotBlank
+    String newPassword;
+
+    @NotBlank
+    String newPasswordConfirm;
+
+    @Override
+    public String toString() {
+        return "PasswordUpdateDto{" +
+                "loginId='" + loginId + '\'' +
+                ", currentPassword='" + currentPassword + '\'' +
+                ", newPassword='" + newPassword + '\'' +
+                ", newPasswordConfirm='" + newPasswordConfirm + '\'' +
+                '}';
+    }
 }
