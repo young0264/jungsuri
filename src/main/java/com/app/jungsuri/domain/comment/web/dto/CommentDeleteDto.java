53ee77c2e32181778dacdf3af2bb817dceca4464
남의영 <ny2485@naver.com>
2023-06-30 13:24:09 +0900

    통합테스트 실패 버그 fix

--- src/main/java/com/app/jungsuri/domain/comment/web/dto/CommentDeleteDto.java
+++ src/main/java/com/app/jungsuri/domain/comment/web/dto/CommentDeleteDto.java
@@ -1,10 +1,14 @@
 package com.app.jungsuri.domain.comment.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDeleteDto {
     Long commentId;
     Long postId;
--- src/test/java/com/app/jungsuri/domain/comment/web/CommentControllerTest.java
+++ src/test/java/com/app/jungsuri/domain/comment/web/CommentControllerTest.java
@@ -9,17 +9,24 @@ import com.app.jungsuri.domain.comment.web.dto.CommentCreateDto;
 import com.app.jungsuri.domain.comment.web.dto.CommentDeleteDto;
 import com.app.jungsuri.domain.comment.web.dto.CommentUpdateDto;
 import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.post.persistence.PostRepository;
 import com.app.jungsuri.domain.post.persistence.PostService;
 import com.app.jungsuri.domain.post.web.dto.PostCreateDto;
 import com.app.jungsuri.infra.MockMvcTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,79 +50,79 @@ class CommentControllerTest {
     AccountService accountService;
 
     @Autowired
+    PostRepository postRepository;
+    @Autowired
     CommentRepository commentRepository;
 
     @Autowired
     ObjectMapper objectMapper;
 
+    Long createdPostId;
+    Long createdCommentId;
+
     @BeforeEach
     @WithMockUser(username = "12", password = "12")
     void init() throws Exception {
-        // given
-        PostCreateDto postCreateDto = new PostCreateDto("제목", "내용", "등록자이름", "12", 0);
+
+        PostCreateDto postCreateDto = new PostCreateDto("게시글 init 제목", "내용", "등록자이름", "12", 0);
 
         //Account Entity , Post Entity 생성
         AccountEntity accountEntity = accountService.findByLoginId("12");
-        postService.createPost(postCreateDto, accountEntity);
+        PostEntity postEntity = postService.createPost(postCreateDto, accountEntity);
+        createdPostId = postEntity.getId();
+
+        CommentCreateDto commentCreateDto = new CommentCreateDto("등록댓글입니다.", "12", postEntity.getId());
+        CommentEntity commentEntity = commentService.createComment(commentCreateDto.toComment(accountEntity, postEntity));
+        createdCommentId = commentEntity.getId();
 
     }
 
 
-    //TODO 동시실행 문제 해결
     @Test
     @WithMockUser(username = "12", password = "12")
     void 댓글등록이_정상적으로_되는지() throws Exception {
 
+        //given
+        AccountEntity accountEntity = accountService.findByLoginId("12");
+        PostEntity postEntity = postRepository.findById(createdPostId).orElseThrow(() -> new IllegalArgumentException("댓글등록 테스트 : id에 해당하는 게시글이 없습니다."));
 
-        // when
+        // when (controller, service 테스트)
         mockMvc.perform(post("/comment/create")
                         .param("content", "댓글입니다.")
                         .param("loginId", "12")
-                        .param("postId", "1")
+                        .param("postId", postEntity.getId().toString())
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/post/1/details"))
-                .andExpect(redirectedUrl("/post/1/details"))
+                .andExpect(view().name("redirect:/post/" + postEntity.getId().toString() + "/details"))
+                .andExpect(redirectedUrl("/post/" + postEntity.getId().toString() + "/details"))
                 .andExpect(authenticated());
+
+        CommentCreateDto commentCreateDto = new CommentCreateDto("댓글 등록 테스트 댓글입니다.", "12", postEntity.getId());
+        CommentEntity commentEntity = commentService.createComment(commentCreateDto.toComment(accountEntity, postEntity));
 
         // then
-        CommentEntity commentEntity = commentRepository.findById(1L).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
-        assertThat(commentEntity.getContent()).isEqualTo("댓글입니다.");
+        assertThat(commentEntity.getContent()).isEqualTo("댓글 등록 테스트 댓글입니다.");
         assertThat(commentEntity.getAuthor()).isEqualTo("남의영");
     }
-
-    //TODO
-    @Test
-    @WithMockUser(username = "12", password = "12")
-    void 댓글등록_실패케이스() throws Exception {
-
-        // when
-        mockMvc.perform(post("/comment/create")
-                        .param("content", "")
-                        .param("postId", "1")
-                        .with(csrf()))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/post/1/details"))
-                .andExpect(authenticated());
-        //then
-    }
-
 
     @Test
     @WithMockUser(username = "12", password = "12")
     void 댓글수정_성공() throws Exception {
 
-        CommentUpdateDto dto = new CommentUpdateDto();
-        dto.setCommentId(1L);
-        dto.setNewComment("수정한 댓글입니다.");
+        //given
+        CommentUpdateDto commentUpdateDto = new CommentUpdateDto();
+        commentUpdateDto.setCommentId(createdCommentId);
+        commentUpdateDto.setNewComment("수정한 댓글입니다.");
 
+        //when
         mockMvc.perform(put("/comment/update")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto))
+                        .content(objectMapper.writeValueAsString(commentUpdateDto))
                         .with(csrf()))
                 .andExpect(status().isOk());
-        CommentEntity commentEntity = commentRepository.findById(1L).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
+        CommentEntity commentEntity = commentRepository.findById(createdCommentId).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
 
+        //then
         assertThat(commentEntity.getContent()).isEqualTo("수정한 댓글입니다.");
     }
 
@@ -124,14 +131,12 @@ class CommentControllerTest {
     void 댓글삭제_성공() throws Exception {
 
         //given
-        CommentDeleteDto dto = new CommentDeleteDto();
-        dto.setCommentId(1L);
-        dto.setPostId(1L);
+        CommentDeleteDto commentDeleteDto = new CommentDeleteDto(createdCommentId, createdPostId);
 
         //when
         mockMvc.perform(delete("/comment/delete")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto))
+                        .content(objectMapper.writeValueAsString(commentDeleteDto))
                         .with(csrf())
                 ).andExpect(status().isOk());
 
@@ -141,8 +146,6 @@ class CommentControllerTest {
 
         //then
         assertThat(exception.getMessage()).isEqualTo("해당 댓글이 없습니다.");
-
     }
-
 
 }
