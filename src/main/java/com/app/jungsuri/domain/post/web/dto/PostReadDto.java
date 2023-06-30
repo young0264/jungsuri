ddc6c624d013a6196857cbd2003d293b6ed3d887
남의영 <ny2485@naver.com>
2023-06-30 19:29:28 +0900

    jooq post 임시 코드

--- src/main/java/com/app/jungsuri/domain/post/web/dto/PostReadDto.java
+++ src/main/java/com/app/jungsuri/domain/post/web/dto/PostReadDto.java
@@ -1,2 +1,37 @@
-package com.app.jungsuri.domain.post.web.dto;public class PostReadDto {
+package com.app.jungsuri.domain.post.web.dto;
+
+import com.app.jungsuri.domain.account.persistence.AccountEntity;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PostReadDto {
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String imagePath;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private String loginId;
+
+    private String author;
+
+    private AccountEntity accountEntity;
+
+    private Integer commentCount;
+
+    private Long likeCount;
+
+    private Long viewCount;
 }
