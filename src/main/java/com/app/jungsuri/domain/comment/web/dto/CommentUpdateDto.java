f51c820d31f0225e070d8bce1965d085a437e3ee
남의영 <ny2485@naver.com>
2023-07-01 18:55:38 +0900

    댓글 수정시 validation 추가

--- src/main/java/com/app/jungsuri/domain/comment/web/dto/CommentUpdateDto.java
+++ src/main/java/com/app/jungsuri/domain/comment/web/dto/CommentUpdateDto.java
@@ -1,12 +1,19 @@
 package com.app.jungsuri.domain.comment.web.dto;
 
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CommentUpdateDto {
+
+    @NotNull(message = "commentId is null")
     Long commentId;
+
+    @NotNull(message="댓글을 작성해주세요.")
+    @Size(min = 5, max = 50, message="댓글은 5자 이상 50자 이하로 작성해주세요.")
     String newComment;
 }
