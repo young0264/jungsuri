e5b092c6cb72b1b10995484fac25202caab78c45
남의영 <ny2485@naver.com>
2023-07-01 18:54:18 +0900

    dto에서 validation 수행

--- src/main/java/com/app/jungsuri/domain/comment/web/dto/CommentCreateDto.java
+++ src/main/java/com/app/jungsuri/domain/comment/web/dto/CommentCreateDto.java
@@ -3,6 +3,7 @@ package com.app.jungsuri.domain.comment.web.dto;
 import com.app.jungsuri.domain.account.persistence.AccountEntity;
 import com.app.jungsuri.domain.comment.model.Comment;
 import com.app.jungsuri.domain.post.persistence.PostEntity;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,10 +17,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class CommentCreateDto {
 
+
+    @NotNull(message="댓글을 작성해주세요.")
+    @Size(min = 5, max = 50, message="댓글은 5자 이상 50자 이하로 작성해주세요.")
     String content;
 
+    @NotNull(message="로그인 정보가 없습니다.")
     String loginId;
 
+    @NotNull(message="게시글 정보가 없습니다.")
     Long postId;
 
 
