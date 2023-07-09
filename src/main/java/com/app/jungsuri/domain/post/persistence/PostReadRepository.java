9f878e1c36c4259f5b981b974a0a5ad6951d9486
남의영 <ny2485@naver.com>
2023-07-09 23:22:33 +0900

    queryDsl 적용. gradle 설정변경

--- src/main/java/com/app/jungsuri/domain/post/persistence/PostReadRepository.java
+++ src/main/java/com/app/jungsuri/domain/post/persistence/PostReadRepository.java
@@ -1,28 +1,2 @@
-package com.app.jungsuri.domain.post.persistence;
-
-import com.app.jungsuri.domain.post.web.dto.PostReadDto;
-import lombok.RequiredArgsConstructor;
-import org.jooq.DSLContext;
-import org.jooq.TableLike;
-import org.springframework.stereotype.Repository;
-
-import static org.jooq.impl.DSL.field;
-import static org.jooq.impl.DSL.table;
-
-//@Repository
-//@RequiredArgsConstructor
-public class PostReadRepository {
-//    private final DSLContext dslContext;
-
-//    public PostReadDto getPost(Long id) {
-////        dslContext.selectFrom(POST)
-////        TableLike<?>[] post;
-//        return (PostReadDto) dslContext.select()
-//                .from(table("post"))
-//                .where(field("id").eq(id)).fetchOne();
-////                .where(post.g)
-////                .where(post)
-////                .fetchOneInto(PostReadDto.class);
-//    }
-
+package com.app.jungsuri.domain.post.persistence;public interface PostReadRepository {
 }
