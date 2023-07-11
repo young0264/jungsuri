351d0359808c43ab3f20033965ece5cf1b01afec
남의영 <ny2485@naver.com>
2023-07-12 02:35:09 +0900

    account 공백 및 주석 코드정리

--- src/main/java/com/app/jungsuri/domain/account/persistence/AccountEntity.java
+++ src/main/java/com/app/jungsuri/domain/account/persistence/AccountEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
