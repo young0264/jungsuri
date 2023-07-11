bcf06ad8ac6b5f3f3f22085d71a156a8367074e8
남의영 <ny2485@naver.com>
2023-07-12 02:45:31 +0900

    jooq db조회 테스트코드

--- src/test/java/com/app/jungsuri/domain/jooqExampleTest.java
+++ src/test/java/com/app/jungsuri/domain/jooqExampleTest.java
@@ -1,2 +1,46 @@
-package com.app.jungsuri.domain;public class jooqExampleTest {
+package com.app.jungsuri.domain;
+
+import com.app.jungsuri.domain.account.persistence.AccountEntity;
+import com.app.jungsuri.domain.account.persistence.AccountRepository;
+import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.post.persistence.PostRepository;
+import com.app.jungsuri.domain.post.persistence.PostService;
+import com.app.jungsuri.domain.post.web.dto.PostCreateDto;
+import com.app.jungsuri.infra.MockMvcTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+
+@MockMvcTest
+public class jooqExampleTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+
+    @Test
+    @WithMockUser(username="12", password="12")
+    public void test() {
+        AccountEntity accountEntity = accountRepository.findByLoginId("12").orElseThrow(RuntimeException::new);
+        PostEntity postEntity = postService.createPost(new PostCreateDto("제목", "내용", accountEntity.getName(), "12", 0), accountEntity);
+
+        /**
+         * jooq로 조회
+         */
+        Assertions.assertThat(postService.findPostEntityById(postEntity.getId()).getContent()).isEqualTo("내용");
+
+        /**
+         * jpa로 조회
+         */
+        Assertions.assertThat(postRepository.findById(postEntity.getId()).orElseThrow(RuntimeException::new).getContent()).isEqualTo("내용");
+
+        Assertions.assertThat(postEntity.getContent()).isEqualTo("내용");
+    }
 }
