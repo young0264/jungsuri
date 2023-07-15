5f2744d58f9c673c319069ab91a9215a7042c60d
남의영 <ny2485@naver.com>
2023-07-16 03:18:51 +0900

    notificationInterceptor postHandle 추가

--- src/main/java/com/app/jungsuri/domain/notification/persistence/NotificationInterceptor.java
+++ src/main/java/com/app/jungsuri/domain/notification/persistence/NotificationInterceptor.java
@@ -1,2 +1,40 @@
-package com.app.jungsuri.domain.notification.persistence;public class NotificationInterceptor {
+package com.app.jungsuri.domain.notification.persistence;
+
+import com.app.jungsuri.domain.account.persistence.AccountEntity;
+import com.app.jungsuri.domain.account.persistence.AccountRepository;
+import com.app.jungsuri.domain.account.web.form.UserAccount;
+import jakarta.mail.Session;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationInterceptor implements HandlerInterceptor {
+
+    private final NotificationRepository notificationRepository;
+    private final AccountRepository accountRepository;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (modelAndView != null && authentication != null && !isRedirectView(modelAndView)) {
+            String loginId = authentication.getName();
+            Long accountId = accountRepository.findIdByLoginId(loginId);
+            long count = notificationRepository.findUncheckedNotificationCountById(accountId);
+            modelAndView.addObject("notificationCount", count);
+        }
+    }
+
+    private boolean isRedirectView(ModelAndView modelAndView) {
+        return modelAndView.getViewName().startsWith("redirect") || modelAndView.getView() instanceof RedirectView;
+    }
 }
