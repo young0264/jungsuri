82646f1f8b0e6cd4684f25c6fd1f8d5d6b05065f
남의영 <ny2485@naver.com>
2023-07-16 03:24:52 +0900

    dsl tables 추가 및 알림갯수 체크 jooq 추가

--- src/main/java/com/app/jungsuri/domain/notification/persistence/NotificationReadRepository.java
+++ src/main/java/com/app/jungsuri/domain/notification/persistence/NotificationReadRepository.java
@@ -3,5 +3,6 @@ package com.app.jungsuri.domain.notification.persistence;
 
 public interface NotificationReadRepository {
 
-    public int findUncheckedNotificationCount(Long accountId);
+    public int findUncheckedNotificationCountById(Long accountId);
+
 }
