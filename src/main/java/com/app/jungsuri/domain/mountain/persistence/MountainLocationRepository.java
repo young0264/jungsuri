4bc2809f7f311fb633d1ff3d1e3af79895eb58b6
남의영 <ny2485@naver.com>
2023-07-20 01:16:02 +0900

    mountain 정보, 소재지 db repository 추가

--- src/main/java/com/app/jungsuri/domain/mountain/persistence/MountainLocationRepository.java
+++ src/main/java/com/app/jungsuri/domain/mountain/persistence/MountainLocationRepository.java
@@ -1,6 +1,8 @@
 package com.app.jungsuri.domain.mountain.persistence;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MountainRepository extends JpaRepository<MountainEntity, Long> {
+public interface MountainLocationRepository extends JpaRepository<MountainLocationEntity, Long> {
+
 }
