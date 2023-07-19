402d5220eefe7d5024acf93bcf927876d698ffe4
남의영 <ny2485@naver.com>
2023-07-20 01:13:01 +0900

    mountain, mountain위치 정보 entity 설계

--- src/main/java/com/app/jungsuri/domain/mountain/persistence/MountainLocationEntity.java
+++ src/main/java/com/app/jungsuri/domain/mountain/persistence/MountainLocationEntity.java
@@ -1,2 +1,27 @@
-package com.app.jungsuri.domain.mountain.persistence;public class MountainLocationEntity {
+package com.app.jungsuri.domain.mountain.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MountainLocationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String location;
+
+    @ManyToOne
+    private MountainEntity mountainEntity;
+
+    public MountainLocationEntity(String mountainAllLocation, MountainEntity mountainEntity) {
+        this.location = mountainAllLocation;
+        this.mountainEntity = mountainEntity;
+    }
 }
