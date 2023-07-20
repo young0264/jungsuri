6c1909bb785abbc9edbacedae62f7256ea9abed8
남의영 <ny2485@naver.com>
2023-07-21 02:50:09 +0900

    mountain controller : mountain list model값 넣어주기

--- src/main/java/com/app/jungsuri/domain/mountain/web/MountainController.java
+++ src/main/java/com/app/jungsuri/domain/mountain/web/MountainController.java
@@ -1,12 +1,16 @@
 package com.app.jungsuri.domain.mountain.web;
 
+import com.app.jungsuri.domain.mountain.persistence.MountainEntity;
 import com.app.jungsuri.domain.mountain.persistence.MountainService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/mountain")
 @RequiredArgsConstructor
@@ -15,7 +19,9 @@ public class MountainController {
     private final MountainService mountainService;
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<MountainEntity> mountainEntities = mountainService.getMountainAllInfo();
+        model.addAttribute("mountainEntities", mountainEntities);
         return "mountain/list";
     }
 }
