e56253b7a5b298fcc7fd64dd8c2fd67b15bab011
남의영 <ny2485@naver.com>
2023-07-21 02:48:39 +0900

    mountain repository 추가

--- src/main/java/com/app/jungsuri/domain/mountain/persistence/MountainRepository.java
+++ src/main/java/com/app/jungsuri/domain/mountain/persistence/MountainRepository.java
@@ -1,6 +1,10 @@
 package com.app.jungsuri.domain.mountain.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface MountainRepository extends JpaRepository<MountainEntity, Long> {
+import java.util.List;
+
+@Transactional
+public interface MountainRepository extends JpaRepository<MountainEntity, Long>, MountainReadRepository {
 }
