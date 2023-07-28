365c596fece1992e922e7eede47109c65e140599
young0264 <ny2485@naver.com>
2023-07-28 15:59:56 +0900

    weather 검색 city dto 추가

--- src/main/java/com/app/jungsuri/domain/weather/web/dto/CityDto.java
+++ src/main/java/com/app/jungsuri/domain/weather/web/dto/CityDto.java
@@ -1,2 +1,12 @@
-package com.app.jungsuri.domain.weather.web.dto;public class CityDto {
+package com.app.jungsuri.domain.weather.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CityDto {
+    private String city;
 }
