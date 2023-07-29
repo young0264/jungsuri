1ea37f05a23a622063d7b4538af07d72251c171f
남의영 <ny2485@naver.com>
2023-07-29 11:28:35 +0900

    url test, urlBuilder 수정 및 weather 도메인 모델로 변경테스트

--- src/test/java/com/app/jungsuri/domain/weather/WeatherApiTest.java
+++ src/test/java/com/app/jungsuri/domain/weather/WeatherApiTest.java
@@ -2,6 +2,7 @@ package com.app.jungsuri.domain.weather;
 
 import com.app.jungsuri.domain.weather.persistence.WeatherEntity;
 import com.app.jungsuri.domain.weather.persistence.WeatherService;
+import com.app.jungsuri.domain.weather.web.dto.WeatherTotalDto;
 import com.app.jungsuri.infra.MockMvcTest;
 import org.assertj.core.api.Assertions;
 import org.json.simple.parser.ParseException;
@@ -11,6 +12,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @MockMvcTest
 public class WeatherApiTest {
@@ -23,26 +25,27 @@ public class WeatherApiTest {
     public void weatherServiceTest()  {
         weatherService.getWeatherData("seoul");
         Assertions.assertThat(weatherService.getWeatherData("seoul")).isNotNull();
-        Assertions.assertThat(weatherService.getWeatherData("seoul").getName()).isEqualTo("Seoul");
+        Assertions.assertThat(weatherService.getWeatherData("seoul").getCity().getName()).isEqualTo("Seoul");
     }
 
     @Test
     @WithMockUser(username = "12", password = "12")
     public void openWeatherAPI_잘받아와지는지() throws IOException, ParseException {
         String BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
-        String apiKey = "발급받은 API key"; // 발급받은 API key
+        String apiKey = System.getenv("weatherAPIKey"); // 발급받은 API key
         StringBuilder urlBuilder = new StringBuilder(BASE_URL);
         try {
             //WeatherEntity로 출력 시작
-            urlBuilder.append("?" + URLEncoder.encode("q", "UTF-8") + "=seoul");
-            urlBuilder.append("&" + URLEncoder.encode("appid", "UTF-8") + "=" + apiKey);
-            urlBuilder.append("&" + URLEncoder.encode("lang", "UTF-8") + "=kr");
-            urlBuilder.append("&" + URLEncoder.encode("units", "UTF-8") + "=metric");
+            urlBuilder.append("?").append(URLEncoder.encode("q", StandardCharsets.UTF_8)).append("=seoul");
+            urlBuilder.append("&").append(URLEncoder.encode("appid", StandardCharsets.UTF_8)).append("=").append(apiKey);
+            urlBuilder.append("&").append(URLEncoder.encode("lang", StandardCharsets.UTF_8)).append("=kr");
+            urlBuilder.append("&").append(URLEncoder.encode("units", StandardCharsets.UTF_8)).append("=metric");
 
             RestTemplate restTemplate = new RestTemplate();
-            WeatherEntity response = restTemplate.getForObject(urlBuilder.toString(), WeatherEntity.class);
-            System.out.println(response.toString());
-            //WeatherEntity로 출력 끝
+            WeatherTotalDto weatherTotalDto = restTemplate.getForObject(urlBuilder.toString(), WeatherTotalDto.class);
+
+            WeatherEntity weatherEntity = weatherService.mapToWeatherData(weatherTotalDto);
+            System.out.println(weatherEntity.toString());
 
         } catch (Exception e) {
             e.printStackTrace();
