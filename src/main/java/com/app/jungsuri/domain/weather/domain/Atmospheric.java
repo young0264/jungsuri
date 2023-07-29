3ee9f629fdaab2db7670015aac393904a688cdbf
남의영 <ny2485@naver.com>
2023-07-29 11:30:39 +0900

    weather 도메인 클래스 생성

--- src/main/java/com/app/jungsuri/domain/weather/domain/Atmospheric.java
+++ src/main/java/com/app/jungsuri/domain/weather/domain/Atmospheric.java
@@ -1,2 +1,30 @@
-package com.app.jungsuri.domain.weather.domain;public class Atmospheric {
+package com.app.jungsuri.domain.weather.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/** 대기 정보 */
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class Atmospheric {
+
+    /** 대기압 (해수면, 해수면 또는 grnd_level 데이터가 없는 경우), hPa */
+    private int pressure;
+
+    /** 습도, % */
+    private float humidity;
+
+    /** 해수면의 대기압, hPa */
+    private float seaLevel;
+
+    /** 지면에서의 대기압, hPa */
+    private float groundLevel;
+
+    /** 가시성 */
+    private int visibility;
 }
--- src/main/java/com/app/jungsuri/domain/weather/domain/Temperature.java
+++ src/main/java/com/app/jungsuri/domain/weather/domain/Temperature.java
@@ -1,2 +1,27 @@
-package com.app.jungsuri.domain.weather.domain;public class Temperature {
+package com.app.jungsuri.domain.weather.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**온도 정보*/
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class Temperature {
+
+    /** 온도. 단위 기본값 : 켈빈, 미터법 : 섭씨, 임페리얼 : 화씨 */
+    private float temp;
+
+    /** 온도. 단위 기본값 : 켈빈, 미터법 : 섭씨, 임페리얼 : 화씨 */
+    private float feelsLike;
+
+    /** 현재 최저 온도.(대규모 대도시 및 도시 지역 내) */
+    private float tempMin;
+
+    /** 현재 최대 온도.(대규모 대도시 및 도시 지역 내)*/
+    private float tempMax;
 }
