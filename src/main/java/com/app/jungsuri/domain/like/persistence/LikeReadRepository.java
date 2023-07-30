0d86ce67d8c2fe9fa8e31d276b70862af4e902bf
남의영 <ny2485@naver.com>
2023-07-30 21:01:53 +0900

    댓글 좋아요 기능관련 read, write repository 수정

--- src/main/java/com/app/jungsuri/domain/like/persistence/LikeReadRepository.java
+++ src/main/java/com/app/jungsuri/domain/like/persistence/LikeReadRepository.java
@@ -3,4 +3,6 @@ package com.app.jungsuri.domain.like.persistence;
 public interface LikeReadRepository {
 
     public boolean isCheckedPostLike(Long accountId, Long postId);
+
+    public boolean isCheckedCommentLike(Long accountId, Long commentId);
 }
