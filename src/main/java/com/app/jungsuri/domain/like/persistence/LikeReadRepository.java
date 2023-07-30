d71a150201767c42fccd392d2c64aaeffe389e15
남의영 <ny2485@naver.com>
2023-07-30 17:08:33 +0900

    좋아요 추가기능  back 코드  추가

--- src/main/java/com/app/jungsuri/domain/like/persistence/LikeReadRepository.java
+++ src/main/java/com/app/jungsuri/domain/like/persistence/LikeReadRepository.java
@@ -2,5 +2,5 @@ package com.app.jungsuri.domain.like.persistence;
 
 public interface LikeReadRepository {
 
-    public boolean isCheckedLike();
+    public boolean isCheckedPostLike(Long accountId, Long postId);
 }
--- src/main/java/com/app/jungsuri/domain/like/persistence/LikeReadRepositoryImpl.java
+++ src/main/java/com/app/jungsuri/domain/like/persistence/LikeReadRepositoryImpl.java
@@ -1,10 +1,8 @@
 package com.app.jungsuri.domain.like.persistence;
 
-import jooq.dsl.tables.Account;
-import jooq.dsl.tables.Likes;
-import jooq.dsl.tables.Post;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
+import static jooq.dsl.tables.Likes.LIKES;
 
 @RequiredArgsConstructor
 public class LikeReadRepositoryImpl implements LikeReadRepository {
@@ -12,10 +10,12 @@ public class LikeReadRepositoryImpl implements LikeReadRepository {
     private final DSLContext dslContext;
 
     @Override
-    public boolean isCheckedLike() {
-        dslContext.select()
-                .from(Likes.LIKES)
-
-        return true;
+    public boolean isCheckedPostLike(Long accountId, Long postId) {
+        return dslContext.fetchExists(
+                dslContext.selectOne()
+                        .from(LIKES)
+                        .where(LIKES.ACCOUNT_ENTITY_ID.eq(accountId), LIKES.POST_ENTITY_ID.eq(postId))
+        );
     }
+
 }
