37f3fb26e9864fe67f538d3f9727c3c5ac735272
남의영 <ny2485@naver.com>
2023-07-30 21:02:35 +0900

    댓글 좋아요 update dto 작성

--- src/main/java/com/app/jungsuri/domain/like/web/dto/CommentLikeUpdateDto.java
+++ src/main/java/com/app/jungsuri/domain/like/web/dto/CommentLikeUpdateDto.java
@@ -1,2 +1,13 @@
-package com.app.jungsuri.domain.like.web.dto;public class CommentLikeUpdateDto {
+package com.app.jungsuri.domain.like.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CommentLikeUpdateDto {
+    private Long accountId;
+    private Long commentId;
 }
