68e5ecacc2e3db2e32b4e61ca67dbc82e0fafd1f
남의영 <ny2485@naver.com>
2023-07-30 20:15:36 +0900

    좋아요 기능 update dto 추가

--- src/main/java/com/app/jungsuri/domain/like/web/dto/LikeUpdateResultDto.java
+++ src/main/java/com/app/jungsuri/domain/like/web/dto/LikeUpdateResultDto.java
@@ -1,2 +1,21 @@
-package com.app.jungsuri.domain.like.web.dto;public class LikeUpdateResultDto {
+package com.app.jungsuri.domain.like.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class LikeUpdateResultDto {
+
+    private int likeCount;
+    private boolean isLike;
+
+    public LikeUpdateResultDto(int likeCount, boolean isLike) {
+        this.likeCount = likeCount;
+        this.isLike = isLike;
+    }
 }
