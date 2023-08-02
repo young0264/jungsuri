08d56ec972ec866c7b8ff57b8010c8d950c81be7
남의영 <ny2485@naver.com>
2023-08-03 00:55:51 +0900

    postTag가 존재하는지. existTag 메서드 테스트

---
+++ src/test/java/com/app/jungsuri/domain/tag/PostTagTest.java
@@ -0,0 +1,41 @@
+package com.app.jungsuri.domain.tag;
+
+import com.app.jungsuri.domain.account.persistence.AccountEntity;
+import com.app.jungsuri.domain.account.persistence.AccountService;
+import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.post.persistence.PostService;
+import com.app.jungsuri.domain.post.web.dto.PostCreateDto;
+import com.app.jungsuri.domain.tag.persistence.Tag;
+import com.app.jungsuri.domain.tag.persistence.repository.PostTagRepository;
+import com.app.jungsuri.infra.MockMvcTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@MockMvcTest
+public class PostTagTest {
+
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostTagRepository postTagRepository;
+
+    @Test
+    void tag가_존재하는지_확인() {
+
+        AccountEntity accountEntity = accountService.findByLoginId("12");
+        PostCreateDto postCreateDto = new PostCreateDto("게시글 등록 제목", "게시글 등록 내용", "게시글 등록 이름", null, 0,   new ArrayList<>(Arrays.asList("태그1", "태그2", "태그3")));
+        postService.createPost(postCreateDto, accountEntity);
+
+        Tag tag = postTagRepository.existTag("태그1").orElse(null);
+
+        Assertions.assertThat(tag).isNotNull();
+
+    }
+}
