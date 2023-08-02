d8607e3befe6749f8e8e453a5da3bc9151f8ada5
young0264 <ny2485@naver.com>
2023-08-02 14:51:29 +0900

    post tag entity추가

--- src/main/java/com/app/jungsuri/domain/tag/persistence/MountainTag.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/MountainTag.java
@@ -1,4 +1,5 @@
 package com.app.jungsuri.domain.tag.persistence;
+
 import com.app.jungsuri.domain.mountain.persistence.MountainEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
@@ -6,11 +7,9 @@ import lombok.*;
 
 @Entity
 @Getter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MountainTag extends Tag{
-    //TODO : tag service를 만들어서 mountain_tag하고 tag 둘 다 저장
 
     @ManyToOne
     private MountainEntity mountainEntity;
