2e22facabf99d72e70eba0208ce2b8c02101dcb5
young0264 <ny2485@naver.com>
2023-08-02 14:54:33 +0900

    tag관련 repository 추가

--- src/main/java/com/app/jungsuri/domain/tag/persistence/repository/PostTagRepository.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/repository/PostTagRepository.java
@@ -1,2 +1,7 @@
-package com.app.jungsuri.domain.tag.persistence.repository;public class PostTagRepository {
+package com.app.jungsuri.domain.tag.persistence.repository;
+
+import com.app.jungsuri.domain.tag.persistence.PostTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostTagRepository extends JpaRepository<PostTag, Long>, PostTagReadRepository{
 }
