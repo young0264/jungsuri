88ae7078c1fb7e7ddae2fad9bcd2bca008664718
남의영 <ny2485@naver.com>
2023-08-03 22:14:04 +0900

    태그 유무에 따른 태그생성 로직 추가

--- src/main/java/com/app/jungsuri/domain/tag/persistence/TagService.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/TagService.java
@@ -1,2 +1,45 @@
-package com.app.jungsuri.domain.tag.persistence;public class TagService {
+package com.app.jungsuri.domain.tag.persistence;
+
+import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.tag.persistence.repository.PostTagRepository;
+import com.app.jungsuri.domain.tag.persistence.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TagService {
+
+    private final TagRepository tagRepository;
+    private final PostTagRepository postTagRepository;
+
+    public Optional<Tag> findByName(String name) {
+        return tagRepository.findByName(name);
+    }
+
+    public void createTags(List<String> tagList, PostEntity postEntity) {
+        for (String tagName : tagList) {
+
+            /** 태그가 있으면 used += 1, 태그가 없으면 태그를 만들고 used += 1 */
+            Optional<Tag> optionalTag = findByName(tagName);
+            optionalTag.ifPresentOrElse(existTag -> {
+                existTag.increaseUsedCount();
+                tagRepository.save(existTag);
+                postTagRepository.save(new PostTag(postEntity, existTag));
+            }, () -> {
+                Tag tag = new Tag(tagName, LocalDateTime.now());
+                tagRepository.save(tag);
+                postTagRepository.save(new PostTag(postEntity, tag));
+            });
+
+        }
+    }
+
 }
