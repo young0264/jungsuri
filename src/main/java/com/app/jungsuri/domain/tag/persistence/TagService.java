0c33c8a37c1adc2007515a8706b366e0361571cc
young0264 <ny2485@naver.com>
2023-08-03 14:46:59 +0900

    tag service 클래스 생성

--- src/main/java/com/app/jungsuri/domain/tag/persistence/TagService.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/TagService.java
@@ -1,2 +1,41 @@
-package com.app.jungsuri.domain.tag.persistence;public class TagService {
+package com.app.jungsuri.domain.tag.persistence;
+
+import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.tag.persistence.repository.PostTagRepository;
+import com.app.jungsuri.domain.tag.persistence.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TagService {
+
+    private final TagRepository tagRepository;
+    private final PostTagRepository postTagRepository;
+
+    public void createTags(List<String> tagList, PostEntity postEntity) {
+        for (String tagName : tagList) {
+            Tag tag = saveTag(tagName);
+            PostTag postTag = new PostTag(postEntity, tag);
+            postTagRepository.save(postTag);
+        }
+    }
+
+    private Tag saveTag(String tagName) {
+        Optional<Tag> optionalTag = tagRepository.findByName(tagName);
+        Tag tag = optionalTag.orElse(null);
+        if (tag == null) {
+            tag = new Tag(tagName, LocalDateTime.now());
+            tag.increaseUsedCount();
+        } else {
+            tag.increaseUsedCount();
+        }
+        tagRepository.save(tag);
+        return tag;
+    }
 }
