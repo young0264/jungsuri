0588a324b1877020db17ff942e07b11b4f057b49
young0264 <ny2485@naver.com>
2023-08-03 14:37:42 +0900

    Tag관련 repository 생성

--- src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagReadRepository.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagReadRepository.java
@@ -1,2 +1,9 @@
-package com.app.jungsuri.domain.tag.persistence.repository;public interface TagReadRepository {
+package com.app.jungsuri.domain.tag.persistence.repository;
+
+import com.app.jungsuri.domain.tag.persistence.Tag;
+import java.util.Optional;
+
+public interface TagReadRepository {
+
+    Optional<Tag> findByName(String name);
 }
