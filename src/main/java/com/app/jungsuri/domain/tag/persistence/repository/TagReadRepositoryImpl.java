09d73789e9ea55abc20add12f55e473b48d488cf
young0264 <ny2485@naver.com>
2023-08-03 14:37:42 +0900

    Tag관련 repository 생성

--- src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagReadRepositoryImpl.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagReadRepositoryImpl.java
@@ -1,2 +1,26 @@
-package com.app.jungsuri.domain.tag.persistence.repository;public class TagReadRepositoryImpl {
+package com.app.jungsuri.domain.tag.persistence.repository;
+
+import com.app.jungsuri.domain.tag.persistence.Tag;
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import java.util.Optional;
+import static jooq.dsl.Tables.TAG;
+
+@RequiredArgsConstructor
+public class TagReadRepositoryImpl implements TagReadRepository{
+
+    private final DSLContext dslContext;
+
+    public Optional<Tag> findByName(String name) {
+        Result<Record> result = dslContext.select()
+                .from(TAG)
+                .where(TAG.NAME.eq(name))
+                .fetch();
+        if (result.isEmpty()) {
+            return Optional.empty();
+        }return Optional.of(result.into(Tag.class).get(0));
+    }
+
 }
