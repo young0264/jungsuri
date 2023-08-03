baa69410afe22fd0b718a6da6af31a2f9bbf6524
young0264 <ny2485@naver.com>
2023-08-03 14:37:08 +0900

    post_tag관련 read repository 생성

--- src/main/java/com/app/jungsuri/domain/tag/persistence/repository/PostTagReadRepository.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/repository/PostTagReadRepository.java
@@ -1,9 +1,5 @@
 package com.app.jungsuri.domain.tag.persistence.repository;
 
-import com.app.jungsuri.domain.tag.persistence.Tag;
-import java.util.Optional;
-
 public interface PostTagReadRepository  {
-    Optional<Tag> existTag(String name);
 
 }
