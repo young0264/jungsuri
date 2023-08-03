be9ff1e3417d610be0e64c569a359f77db236bbf
남의영 <ny2485@naver.com>
2023-08-03 22:09:02 +0900

    table과 매핑할 @column어노테이션 명시

--- src/main/java/com/app/jungsuri/domain/tag/persistence/Tag.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/Tag.java
@@ -17,15 +17,19 @@ public class Tag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="name", unique = true)
     private String name;
 
+    @Column(name="used_count")
     private int usedCount = 0;
 
+    @Column(name="created_at")
     private LocalDateTime createdAt;
 
     public Tag(String name, LocalDateTime createdAt) {
         this.name = name;
         this.createdAt = createdAt;
+        this.usedCount = 1;
     }
 
     public void increaseUsedCount() {
