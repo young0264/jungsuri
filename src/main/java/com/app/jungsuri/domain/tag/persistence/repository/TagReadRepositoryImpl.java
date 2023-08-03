36217f13b76c8082f22b21065b8ea08139d28627
남의영 <ny2485@naver.com>
2023-08-03 22:12:23 +0900

    find tag by name : tag repository에 추가

--- src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagReadRepositoryImpl.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagReadRepositoryImpl.java
@@ -1,2 +1,21 @@
-package com.app.jungsuri.domain.tag.persistence.repository;public class TagReadRepositoryImpl {
+package com.app.jungsuri.domain.tag.persistence.repository;
+
+import com.app.jungsuri.domain.tag.persistence.Tag;
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import java.util.Optional;
+import static jooq.dsl.tables.Tag.TAG;
+
+@RequiredArgsConstructor
+public class TagReadRepositoryImpl implements TagReadRepository {
+
+    private final DSLContext dslContext;
+
+    public Optional<Tag> findByName(String name) {
+         return dslContext.select()
+                .from(TAG)
+                .where(TAG.NAME.eq(name))
+                .fetchOptionalInto(Tag.class);
+
+    }
 }
--- src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagRepository.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/repository/TagRepository.java
@@ -3,6 +3,6 @@ package com.app.jungsuri.domain.tag.persistence.repository;
 import com.app.jungsuri.domain.tag.persistence.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {
+public interface TagRepository extends JpaRepository<Tag, Long>, TagReadRepository {
 
 }
