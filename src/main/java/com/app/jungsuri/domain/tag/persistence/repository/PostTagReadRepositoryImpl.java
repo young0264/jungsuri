75daaa5a0e7470c77466b05dc8e2980655565efd
young0264 <ny2485@naver.com>
2023-08-03 14:37:08 +0900

    post_tag관련 read repository 생성

--- src/main/java/com/app/jungsuri/domain/tag/persistence/repository/PostTagReadRepositoryImpl.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/repository/PostTagReadRepositoryImpl.java
@@ -1,23 +1,9 @@
 package com.app.jungsuri.domain.tag.persistence.repository;
 
-import com.app.jungsuri.domain.tag.persistence.Tag;
 import lombok.RequiredArgsConstructor;
-import org.jooq.DSLContext;
-import java.util.Optional;
-
-import static jooq.dsl.tables.Tag.TAG;
 
 @RequiredArgsConstructor
 public class PostTagReadRepositoryImpl implements PostTagReadRepository{
-
-    private final DSLContext dslContext;
-
-    public Optional<Tag> existTag(String name) {
-        return dslContext.select()
-                .from(TAG)
-                .where(TAG.NAME.eq(name))
-                .fetchOptionalInto(Tag.class);
-    }
 
 
 }
