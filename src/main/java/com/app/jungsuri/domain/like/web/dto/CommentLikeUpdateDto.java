ac57474248f7e5c61c81a8c7c06d0eb41aa57b53
남의영 <ny2485@naver.com>
2023-08-06 21:30:46 +0900

    comment like 좋아요추가시 post id추가

--- src/main/java/com/app/jungsuri/domain/like/web/dto/CommentLikeUpdateDto.java
+++ src/main/java/com/app/jungsuri/domain/like/web/dto/CommentLikeUpdateDto.java
@@ -1,13 +1,14 @@
 package com.app.jungsuri.domain.like.web.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentLikeUpdateDto {
     private Long accountId;
+    private Long postId;
     private Long commentId;
 }
