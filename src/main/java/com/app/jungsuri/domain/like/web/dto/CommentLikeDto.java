19360b2729b12e114f5e18a746005bfec1ee7595
남의영 <ny2485@naver.com>
2023-08-07 00:19:02 +0900

    comment 좋아요 표시를 한 comment Id를 가져오기 위한 dto조건

---
+++ src/main/java/com/app/jungsuri/domain/like/web/dto/CommentLikeDto.java
@@ -0,0 +1,11 @@
+package com.app.jungsuri.domain.like.web.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class CommentLikeDto {
+    private Long accountId;
+    private Long postId;
+}
