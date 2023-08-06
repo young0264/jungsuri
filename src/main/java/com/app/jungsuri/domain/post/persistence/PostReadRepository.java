2cb4aeeb317649bd35bedaf0afaac5507d016f26
남의영 <ny2485@naver.com>
2023-08-06 13:56:37 +0900

    태그를 이용한 게시글 비동기 검색 기능 추가

--- src/main/java/com/app/jungsuri/domain/post/persistence/PostReadRepository.java
+++ src/main/java/com/app/jungsuri/domain/post/persistence/PostReadRepository.java
@@ -1,6 +1,10 @@
 package com.app.jungsuri.domain.post.persistence;
 
+import java.util.List;
+
 public interface PostReadRepository {
 
     PostEntity findPostEntityById(Long id);
+
+    List<PostEntity> findAllByTags(List<String> searchTags);
 }
