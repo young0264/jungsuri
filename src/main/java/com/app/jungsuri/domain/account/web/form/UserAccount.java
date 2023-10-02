644e5d51f64bd4e2173c378d49a07d6e125086c1
남의영 <ny2485@naver.com>
2023-10-02 17:12:25 +0900

    authority user(기본값) -> 선택된 authority로 수정

--- src/main/java/com/app/jungsuri/domain/account/web/form/UserAccount.java
+++ src/main/java/com/app/jungsuri/domain/account/web/form/UserAccount.java
@@ -2,17 +2,19 @@ package com.app.jungsuri.domain.account.web.form;
 
 import com.app.jungsuri.domain.account.persistence.AccountEntity;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import java.util.List;
 
 @Getter
+@Slf4j
 public class UserAccount extends User {
 
     private AccountEntity accountEntity;
 
     public UserAccount(AccountEntity accountEntity) {
-        super(accountEntity.getLoginId(), accountEntity.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        super(accountEntity.getLoginId(), accountEntity.getPassword(), List.of(new SimpleGrantedAuthority(accountEntity.getUserRole().toString())));
         this.accountEntity = accountEntity;
     }
 
