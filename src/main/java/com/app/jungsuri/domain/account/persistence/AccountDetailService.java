7ef233dc7c3a32266c84d9d1b4f0a3eda28190aa
남의영 <ny2485@naver.com>
2023-10-02 17:41:22 +0900

    UserDetailsService 구현체 분리

--- src/main/java/com/app/jungsuri/domain/account/persistence/AccountDetailService.java
+++ src/main/java/com/app/jungsuri/domain/account/persistence/AccountDetailService.java
@@ -1,2 +1,38 @@
-package com.app.jungsuri.domain.account.persistence;public class AccountDetailService {
+package com.app.jungsuri.domain.account.persistence;
+
+import com.app.jungsuri.domain.account.web.form.UserAccount;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+@Slf4j
+public class AccountDetailService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        AccountEntity accountEntity = accountRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("로그인 아이디에 해당하는 유저는 존재하지 않습니다."));
+        accountEntity.setAuthorities(List.of(new SimpleGrantedAuthority(accountEntity.getUserRole().toString())));
+
+        return new UserAccount(accountEntity);
+
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
