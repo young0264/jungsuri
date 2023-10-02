70d4c3fec085a0690ecc0bdb0168f98667e4eef1
남의영 <ny2485@naver.com>
2023-10-02 17:18:04 +0900

    에러가 났을때 redirect 추가

--- src/main/java/com/app/jungsuri/domain/account/web/EmailController.java
+++ src/main/java/com/app/jungsuri/domain/account/web/EmailController.java
@@ -28,6 +28,7 @@ public class EmailController {
         AccountEntity accountEntity = accountService.findByLoginId(principal.getName());
         if (!accountEntity.isValidToken(token)) {
             redirectAttributes.addAttribute("error", "wrong.email token");
+            return MAIN_REDIRECT_VIEW;
         }
         accountService.completeSignUp(accountEntity);
         return MAIN_REDIRECT_VIEW;
