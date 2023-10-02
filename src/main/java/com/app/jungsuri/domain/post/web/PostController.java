3d91f4c16f456d67573d591c4297e47de64d0d57
남의영 <ny2485@naver.com>
2023-10-02 21:45:20 +0900

    tag bug fix

--- src/main/java/com/app/jungsuri/domain/post/web/PostController.java
+++ src/main/java/com/app/jungsuri/domain/post/web/PostController.java
@@ -6,34 +6,45 @@ import com.app.jungsuri.domain.comment.web.dto.CommentCreateDto;
 import com.app.jungsuri.domain.post.persistence.PostService;
 import com.app.jungsuri.domain.post.web.dto.PostCreateDto;
 import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.post.web.dto.PostSearchDto;
+import com.app.jungsuri.domain.tag.persistence.TagService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.security.Principal;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/post")
 @RequiredArgsConstructor
-@Slf4j
 public class PostController {
 
+    private final TagService tagService;
     private final PostService postService;
     private final AccountService accountService;
 
-    // 게시글 목록 조회
+    /** 게시글 태그 (or 조건) 검색 */
+    @PostMapping("/list")
+    public String search(@RequestBody PostSearchDto postSearchDto, Model model) {
+        List<PostEntity> postListByTags = postService.getPostListByTags(postSearchDto.getSearchTags());
+        model.addAttribute("postList", postListByTags);
+        return "post/list :: #post_list ";
+    }
+
+    /** 게시글 목록 조회 */
     @GetMapping("/list")
     public String list(Model model) {
         List<PostEntity> postList = postService.getPostList();
         model.addAttribute("postList", postList);
+        model.addAttribute("tagList", tagService.getTagNameList());
         return "post/list";
     }
 
-    // 게시글 등록 화면 이동
+    /** 게시글 등록 화면 이동 */
     @GetMapping("")
     public String write(Model model) {
         model.addAttribute("postCreateDto", new PostCreateDto());
@@ -41,29 +52,32 @@ public class PostController {
         return "post/form";
     }
 
-    // 게시글 등록 처리
+    /** 게시글 등록 처리 */
     @PostMapping("")
     public String write(@ModelAttribute("postCreateDto") PostCreateDto postCreateDto, Principal principal, RedirectAttributes redirectAttributes){
         AccountEntity accountEntity = accountService.findByLoginId(principal.getName());
-        postService.createPost(postCreateDto, accountEntity);
+        PostEntity postEntity = postService.createPost(postCreateDto, accountEntity);
+        tagService.createTags(postCreateDto.getTagList(), postEntity);
+
         redirectAttributes.addFlashAttribute("message", "게시물이 성공적으로 등록되었습니다.");
         return "redirect:/post/list";
     }
 
-    // 게시글 상세 페이지
+    /** 게시글 상세 페이지 */
     @GetMapping("/{postId}/details")
     public String view(@PathVariable Long postId, Model model, Principal principal) {
         PostEntity postEntity = postService.getPostEntity(postId);
         AccountEntity accountEntity = accountService.findByLoginId(principal.getName());
 
         model.addAttribute("accountEntity", accountEntity);
+        model.addAttribute("tagList", tagService.getTagNameList());
         model.addAttribute("postEntity", postEntity);
         model.addAttribute("commentCreateDto", new CommentCreateDto());
         model.addAttribute("commentList", postEntity.getCommentList());
         return "post/details";
     }
 
-    // 게시글 수정 화면 이동
+    /** 게시글 수정 화면 이동 */
     @GetMapping("/{postId}/updateView")
     public String update(@PathVariable Long postId, Model model) {
         PostEntity postEntity = postService.getPostEntity(postId);
@@ -72,7 +86,7 @@ public class PostController {
         return "post/form";
     }
 
-    // 게시글 수정 처리
+    /** 게시글 수정 처리 */
     @PatchMapping("/{postId}")
     public String edit(@PathVariable Long postId, @ModelAttribute("postEntity") PostEntity existingPost, RedirectAttributes redirectAttributes) {
         postService.updatePost(existingPost, postId);
@@ -80,7 +94,7 @@ public class PostController {
         return "redirect:/post/list";
     }
 
-    // 게시글 삭제 처리
+    /** 게시글 삭제 처리 */
     @DeleteMapping("/{postId}")
     public String delete(@PathVariable Long postId, RedirectAttributes redirectAttributes){
         postService.deletePost(postId);
--- src/main/java/com/app/jungsuri/domain/tag/persistence/Tag.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/Tag.java
@@ -17,19 +17,26 @@ public class Tag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="name", unique = true)
     private String name;
 
-    @Builder.Default
+    @Column(name="used_count")
     private int usedCount = 0;
 
+    @Column(name="created_at")
     private LocalDateTime createdAt;
 
     public Tag(String name, LocalDateTime createdAt) {
         this.name = name;
         this.createdAt = createdAt;
+        this.usedCount = 1;
     }
 
     public void increaseUsedCount() {
         this.usedCount += 1;
+    }
+
+    public String toString() {
+        return "Tag(id=" + this.getId() + ", name=" + this.getName() + ", usedCount=" + this.getUsedCount() + ", createdAt=" + this.getCreatedAt() + ")";
     }
 }
