b689921d5622b222b02616a9dbe9cbc9c4e60f2f
남의영 <ny2485@naver.com>
2023-10-03 18:49:45 +0900

    comment service 주석, findtop5 코드 추가

--- src/main/java/com/app/jungsuri/domain/comment/persistence/CommentService.java
+++ src/main/java/com/app/jungsuri/domain/comment/persistence/CommentService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,12 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final CommentReadRepository commentReadRepository;
 
+    /** 댓글 생성 */
     public CommentEntity createComment(Comment comment) {
         comment.getPostEntity().increaseCommentCount();
         return commentRepository.save(comment.toEntity());
     }
 
+    /** 댓글 수정 */
     public void updateComment(Long commentId, String newComment) {
         commentRepository.findById(commentId).orElseGet(() -> {
             log.error("Comment not found");
@@ -27,13 +31,20 @@ public class CommentService {
         }).updateComment(newComment);
     }
 
+    /** 댓글 삭제 */
     public void deleteComment(Long commentId, PostEntity postEntity) {
         postEntity.decreaseCommentCount();
         commentRepository.deleteById(commentId);
     }
 
+    /** 댓글 id(pk)로 commentEntity 가져오기 */
     public CommentEntity getComment(Long commentId) {
         return commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다."));
+    }
+
+    /** 최신 댓글 5개 가져오기 */
+    public List<CommentEntity> getRecentCommentList() {
+        return commentReadRepository.findTop5ByOrderByCreatedAtDesc();
     }
 
 }
