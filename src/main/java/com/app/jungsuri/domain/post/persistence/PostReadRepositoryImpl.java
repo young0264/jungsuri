6b067504906e24a9b5648a52d5bc956560889f07
남의영 <ny2485@naver.com>
2023-10-05 00:22:06 +0900

    post pagination repository 추가

--- src/main/java/com/app/jungsuri/domain/post/persistence/PostReadRepositoryImpl.java
+++ src/main/java/com/app/jungsuri/domain/post/persistence/PostReadRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.app.jungsuri.domain.post.persistence;
 
+import com.app.jungsuri.infra.pagination.PostPage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -8,11 +10,20 @@ import static jooq.dsl.tables.Post.POST;
 import static jooq.dsl.tables.PostTag.POST_TAG;
 import static jooq.dsl.tables.Tag.TAG;
 
+@Slf4j
 @Repository //제거 혹은 repository라는 것을 알려주는 용도의 annotation
 @RequiredArgsConstructor
 public class PostReadRepositoryImpl implements PostReadRepository {
     private final DSLContext dslContext;
 
+    /** 현재 등록된 모든 게시물의 갯수를 반환 */
+    public int getPostCount() {
+        return dslContext.selectCount()
+                .from(POST)
+                .fetchOneInto(Integer.class);
+    }
+
+    /** id에 해당하는 post 가져오기 */
     public PostEntity findPostEntityById(Long id) {
         return dslContext.select()
                         .from(POST)
@@ -20,6 +31,8 @@ public class PostReadRepositoryImpl implements PostReadRepository {
                         .fetchOneInto(PostEntity.class);
     }
 
+
+    /** Tag에 해당하는 post 가져오기 */
     public List<PostEntity> findAllByTags(List<String> searchTags) {
         var tagSubQuery = dslContext.select(TAG.ID)
                 .from(TAG)
@@ -33,11 +46,21 @@ public class PostReadRepositoryImpl implements PostReadRepository {
                 .fetchInto(PostEntity.class);
     }
 
+    /** 좋아요 갯수가 많은 5개의 게시글 가져오기 */
     public List<PostEntity> findTop5ByLikeCountAsc() {
         return dslContext.select()
                 .from(POST)
                 .orderBy(POST.LIKE_COUNT.desc())
                 .limit(5)
+                .fetchInto(PostEntity.class);
+    }
+
+    /** pagination 적용된 post list 가져오기 */
+    public List<PostEntity> findPostListByPagination(int startRowNum) {
+        return dslContext.select()
+                .from(POST)
+                .orderBy(POST.CREATED_AT.desc())
+                .limit(startRowNum-1, PostPage.PAGE_ROW_SIZE.getValue())
                 .fetchInto(PostEntity.class);
     }
 
