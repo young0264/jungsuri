62c5219199fd6b83203b13e824370327a14936a1
남의영 <ny2485@naver.com>
2023-10-09 18:13:24 +0900

    post create test 데이터 삽입

--- src/main/java/com/app/jungsuri/mainController.java
+++ src/main/java/com/app/jungsuri/mainController.java
@@ -1,10 +1,15 @@
 package com.app.jungsuri;
 
+import com.app.jungsuri.domain.account.persistence.AccountEntity;
 import com.app.jungsuri.domain.account.persistence.AccountService;
 import com.app.jungsuri.domain.account.web.UserRole;
 import com.app.jungsuri.domain.account.web.form.SignUpForm;
 import com.app.jungsuri.domain.mountain.persistence.MountainLocationService;
 import com.app.jungsuri.domain.mountain.persistence.MountainService;
+import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.post.persistence.PostRepository;
+import com.app.jungsuri.domain.post.persistence.PostService;
+import com.app.jungsuri.domain.post.web.dto.PostCreateDto;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +18,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -21,6 +27,7 @@ public class mainController {
 
     private final AccountService accountService;
     private final MountainService mountainService;
+    private final PostRepository postRepository;
     private final MountainLocationService mountainLocationService;
 
     @PostConstruct
@@ -41,6 +48,19 @@ public class mainController {
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
+        }
+    }
+
+    @PostConstruct
+    public void postTestInit() {
+        AccountEntity accountEntity = accountService.findByLoginId("12");
+
+//        PostEntity postEntity = createPost(PostCreateDto postCreateDto, AccountEntity accountEntity)
+
+        for (int i = 0; i < 7; i++) {
+            PostCreateDto postCreateDto = new PostCreateDto("게시글 제목"+i, "내용"+i, "등록자이름"+i, "12"+i, 0, new ArrayList<>());
+            PostEntity postEntity = new PostEntity(postCreateDto, accountEntity);
+            postRepository.save(postEntity);
         }
     }
 
