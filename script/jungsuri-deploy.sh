2375edab913c0d60085458b49958d84b9e2049d9
남의영 <ny2485@naver.com>
2023-10-16 22:24:37 +0900

    deploy sh test

--- script/jungsuri-deploy.sh
+++ script/jungsuri-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/jungsuri"
-JAR_FILE="$PROJECT_ROOT/build/libs/jungsuri-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/jungsuri-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -11,15 +11,12 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-sudo cp $PROJECT_ROOT/jungsuri-0.0.1-SNAPSHOT.jar $PROJECT_ROOT
+cp $PROJECT_ROOT/jungsuri-0.0.1-SNAPSHOT.jar
 
-# jar 파일 실행ㄷㅋ
+# jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 #프로파일 명시
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
-#$ chmod +x ./deploy.sh  // 권한 부여
-#$ ./deploy.sh           // 실행
-#$ vim nohup.out         // 로그 확인
