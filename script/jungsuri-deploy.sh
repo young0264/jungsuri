c8ee4a2d538b0ba9d1b5824218399ff02744e74c
남의영 <ny2485@naver.com>
2023-10-16 22:17:40 +0900

    deploy sh 파일 수정

--- script/jungsuri-deploy.sh
+++ script/jungsuri-deploy.sh
@@ -11,12 +11,15 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/jungsuri-0.0.1-SNAPSHOT.jar
+sudo cp $PROJECT_ROOT/jungsuri-0.0.1-SNAPSHOT.jar $PROJECT_ROOT
 
-# jar 파일 실행
+# jar 파일 실행ㄷㅋ
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 #프로파일 명시
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+#$ chmod +x ./deploy.sh  // 권한 부여
+#$ ./deploy.sh           // 실행
+#$ vim nohup.out         // 로그 확인
