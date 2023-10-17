4aeed897f91e2dd9f713b03e7a05072b9371b65e
남의영 <ny2485@naver.com>
2023-10-18 00:50:05 +0900

    환경설정 관련 api swagger 적용

--- src/main/java/com/app/jungsuri/domain/account/web/SettingsController.java
+++ src/main/java/com/app/jungsuri/domain/account/web/SettingsController.java
@@ -8,6 +8,8 @@ import com.app.jungsuri.domain.account.web.dto.PasswordUpdateDto;
 import com.app.jungsuri.domain.mountain.persistence.MountainService;
 import com.app.jungsuri.domain.notification.persistence.NotificationEntity;
 import com.app.jungsuri.domain.notification.persistence.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/settings")
 @RequiredArgsConstructor
+@Tag(name = "환경설정 관련 API", description = "계정 환경설정(settings) 관련 API")
 public class SettingsController {
 
     private final AccountService accountService;
@@ -30,6 +33,7 @@ public class SettingsController {
     private final NotificationService notificationService;
 
     @GetMapping("/alarm")
+    @Operation(summary = "알림 페이지 조회", description = "알림 페이지를 조회합니다.")
     public String showAlarmSettingPage(Model model, Principal principal) {
         String loginId = principal.getName();
         AccountEntity accountEntity = accountService.findByLoginId(loginId);
@@ -41,6 +45,7 @@ public class SettingsController {
     }
 
     @GetMapping("/profile")
+    @Operation(summary = "프로필 페이지 조회", description = "프로필 페이지 조회를 조회합니다.")
     public String showProfile(Model model, Principal principal) {
         String loginId = principal.getName();
         AccountEntity accountEntity = accountService.findByLoginId(loginId);
@@ -51,6 +56,7 @@ public class SettingsController {
 
     //TODO PATCH ? PUT ?
     @PutMapping("/password")
+    @Operation(summary = "비밀번호 수정 처리", description = "비밀번호를 수정합니다.")
     public ResponseEntity updatePassword(@RequestBody PasswordUpdateDto passwordUpdateDto) {
         try {
             settingsService.updatePassword(passwordUpdateDto);
@@ -61,7 +67,8 @@ public class SettingsController {
     }
 
     @GetMapping("/admin")
-    public String showAdminSettingPage(Model model,Principal principal) {
+    @Operation(summary = "관리자 페이지 조회", description = "관리자 페이지를 조회합니다.")
+    public String showAdminSettingPage(Model model, Principal principal) {
         List<String> userLoginIdList = accountService.getAllUsersLoginId();
         List<String> mountainNameList = mountainService.findAllMountainsName();
         AccountEntity accountEntity = accountService.findByLoginId(principal.getName());
@@ -70,6 +77,7 @@ public class SettingsController {
     }
 
     @PatchMapping("/mountain-exp")
+    @Operation(summary = "산에 따른 경험치 수정", description = "산(mountain)에 따른 경험치를 수정합니다.")
     public ResponseEntity updateMountainExp(@RequestBody MountainExpUpdateDto mountainExpUpdateDto) {
         try {
             settingsService.updateMountainExp(mountainExpUpdateDto);
