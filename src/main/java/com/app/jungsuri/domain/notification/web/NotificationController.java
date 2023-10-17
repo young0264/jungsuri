2aa2b3cbbf5ba883f3d9dcf9872226fba23821db
남의영 <ny2485@naver.com>
2023-10-18 01:59:45 +0900

    alarm 알림 api swagger 등록

--- src/main/java/com/app/jungsuri/domain/notification/web/NotificationController.java
+++ src/main/java/com/app/jungsuri/domain/notification/web/NotificationController.java
@@ -1,6 +1,8 @@
 package com.app.jungsuri.domain.notification.web;
 
 import com.app.jungsuri.domain.notification.persistence.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/notification")
 @RequiredArgsConstructor
+@Tag(name = "알림 관련 API", description = "알림(alarm) 관련 API")
 public class NotificationController {
 
     private final NotificationService notificationService;
 
     @PutMapping("/check")
     @ResponseBody
+    @Operation(summary = "알림 체크", description = "알림이 현재 있는지를 체크합니다.")
     public ResponseEntity checkNotification(@RequestBody CheckDto checkDto) {
         notificationService.checkNotification(checkDto.getNotificationId());
         return ResponseEntity.ok().build();
