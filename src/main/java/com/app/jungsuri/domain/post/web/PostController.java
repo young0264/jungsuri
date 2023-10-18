a3156041948e33a5574914c3f04534f4e194b303
남의영 <ny2485@naver.com>
2023-10-18 15:56:37 +0900

    게시글 삭제시 연관관계 tag 데이터 제거

--- src/main/java/com/app/jungsuri/domain/post/web/PostController.java
+++ src/main/java/com/app/jungsuri/domain/post/web/PostController.java
@@ -33,46 +33,6 @@ public class PostController {
     private final AccountService accountService;
     private final CommentService commentService;
 
-    /** 게시글 태그 (or 조건) 검색 */
-    @PostMapping("/list")
-    @Operation(summary = "게시글 태그 검색", description = "searchTags 정보에 따라 태그가 달린 게시글을 조회합니다.")
-    public String search(@RequestBody PostSearchDto postSearchDto, Model model) {
-
-        List<PostEntity> postListByTags = postService.getPostListByTags(postSearchDto.getSearchTags());
-        model.addAttribute("postList", postListByTags);
-        return "post/list :: #post_list ";
-    }
-
-    /** 게시글 목록 조회 */
-    @GetMapping("/list")
-    @Operation(summary = "게시글 조회", description = "페이지 네이션이 적용된 게시글을 조회합니다.")
-    public String list(@RequestParam(required = false, defaultValue = "1") int currentPageNumber , Model model) {
-
-        List<PostEntity> postListByPagination = postService.getPostListByPagination(currentPageNumber);
-        List<PostEntity> postListByTop5 = postService.getTop5ListByLikeCount();
-        List<CommentEntity> recentCommentListTop5 = commentService.getRecentCommentList();
-
-        model.addAttribute("beforePageNum", postService.getBeforePageNum(currentPageNumber));
-        model.addAttribute("nextPageNum", postService.getEndPageNum(currentPageNumber));
-        model.addAttribute("pagingCount", postService.getPagingNumber());
-        model.addAttribute("postList", postListByPagination);
-        model.addAttribute("tagList", tagService.getTagNameList());
-        model.addAttribute("postListByTop5", postListByTop5);
-        model.addAttribute("recentCommentListTop5", recentCommentListTop5);
-        return "post/list";
-    }
-
-
-
-
-    /** 게시글 등록 화면 이동 */
-    @GetMapping("")
-    @Operation(summary = "게시글 등록 페이지 조회", description = "게시글 작성 페이지를 조회합니다.")
-    public String write(Model model) {
-        model.addAttribute("postCreateDto", new PostCreateDto());
-        model.addAttribute("mode", "create");
-        return "post/form";
-    }
 
     /** 게시글 등록 처리 */
     @PostMapping("")
@@ -86,8 +46,37 @@ public class PostController {
         return "redirect:/post/list";
     }
 
+    /** 게시글 등록 화면 이동 */
+    @GetMapping("")
+    @Operation(summary = "게시글 등록 페이지 조회", description = "게시글 작성 페이지를 조회합니다.")
+    public String write(Model model) {
+        model.addAttribute("postCreateDto", new PostCreateDto());
+        model.addAttribute("mode", "create");
+        return "post/form";
+    }
+
+    /** 게시글 수정 처리 */
+    @PatchMapping("/{postId}")
+    @Operation(summary="게시글 수정 처리", description="게시글 수정 처리")
+    public String edit(@PathVariable Long postId, @ModelAttribute("postEntity") PostEntity existingPost, RedirectAttributes redirectAttributes) {
+        postService.updatePost(existingPost, postId);
+        redirectAttributes.addFlashAttribute("message", "/titan/live/assets/");
+        return "redirect:/post/list";
+    }
+
+    /** 게시글 삭제 처리 */
+    @DeleteMapping("/{postId}")
+    @Operation(summary="게시글 삭제 처리", description="게시글 삭제 처리")
+    public String delete(@PathVariable Long postId, RedirectAttributes redirectAttributes){
+        tagService.deleteTag(postId);
+        postService.deletePost(postId);
+        System.out.println("post delete complete");
+        redirectAttributes.addFlashAttribute("delete_message", "게시물이 성공적으로 삭제되었습니다.");
+        return "redirect:/post/list";
+    }
+
     /** 게시글 상세 페이지 */
-    @GetMapping("/{postId}/details")
+    @GetMapping("/{postId}")
     @Operation(summary = "게시글 상세 페이지 조회", description = "하나의 게시글을 조회할 수 있는 페이지로 이동합니다.")
     public String view(@PathVariable Long postId, Model model, Principal principal) {
         PostEntity postEntity = postService.getPostEntity(postId);
@@ -116,22 +105,35 @@ public class PostController {
         return "post/form";
     }
 
-    /** 게시글 수정 처리 */
-    @PatchMapping("/{postId}")
-    @Operation(summary="게시글 수정 처리", description="게시글 수정 처리")
-    public String edit(@PathVariable Long postId, @ModelAttribute("postEntity") PostEntity existingPost, RedirectAttributes redirectAttributes) {
-        postService.updatePost(existingPost, postId);
-        redirectAttributes.addFlashAttribute("message", "/titan/live/assets/");
-        return "redirect:/post/list";
+    /** 게시글 태그 (or 조건) 검색 */
+    @PostMapping("/list")
+    @Operation(summary = "게시글 태그 검색", description = "searchTags 정보에 따라 태그가 달린 게시글을 조회합니다.")
+    public String search(@RequestBody PostSearchDto postSearchDto, Model model) {
+
+        List<PostEntity> postListByTags = postService.getPostListByTags(postSearchDto.getSearchTags());
+        model.addAttribute("postList", postListByTags);
+        return "post/list :: #post_list ";
     }
 
-    /** 게시글 삭제 처리 */
-    @DeleteMapping("/{postId}")
-    @Operation(summary="게시글 삭제 처리", description="게시글 삭제 처리")
-    public String delete(@PathVariable Long postId, RedirectAttributes redirectAttributes){
-        postService.deletePost(postId);
-        System.out.println("post delete complete");
-        redirectAttributes.addFlashAttribute("delete_message", "게시물이 성공적으로 삭제되었습니다.");
-        return "redirect:/post/list";
+
+    /** 게시글 목록 조회 */
+    @GetMapping("/list")
+    @Operation(summary = "게시글 조회", description = "페이지 네이션이 적용된 게시글을 조회합니다.")
+    public String list(@RequestParam(required = false, defaultValue = "1") int currentPageNumber , Model model) {
+
+        List<PostEntity> postListByPagination = postService.getPostListByPagination(currentPageNumber);
+        List<PostEntity> postListByTop5 = postService.getTop5ListByLikeCount();
+        List<CommentEntity> recentCommentListTop5 = commentService.getRecentCommentList();
+
+        model.addAttribute("beforePageNum", postService.getBeforePageNum(currentPageNumber));
+        model.addAttribute("nextPageNum", postService.getEndPageNum(currentPageNumber));
+        model.addAttribute("pagingCount", postService.getPagingNumber());
+        model.addAttribute("postList", postListByPagination);
+        model.addAttribute("tagList", tagService.getTagNameList());
+        model.addAttribute("postListByTop5", postListByTop5);
+        model.addAttribute("recentCommentListTop5", recentCommentListTop5);
+        return "post/list";
     }
+
+
 }
