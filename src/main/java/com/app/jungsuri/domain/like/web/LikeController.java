ecf2cbbe89b4a3590413b1e19a519a5bb4f61fb8
남의영 <ny2485@naver.com>
2023-10-20 03:11:54 +0900

    like controller 좋아요 도메인 swagger 적용

--- src/main/java/com/app/jungsuri/domain/like/web/LikeController.java
+++ src/main/java/com/app/jungsuri/domain/like/web/LikeController.java
@@ -5,6 +5,8 @@ import com.app.jungsuri.domain.like.web.dto.CommentLikeDto;
 import com.app.jungsuri.domain.like.web.dto.CommentLikeUpdateDto;
 import com.app.jungsuri.domain.like.web.dto.LikeUpdateResultDto;
 import com.app.jungsuri.domain.like.web.dto.PostLikeUpdateDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +20,20 @@ import java.util.List;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@Tag(name = "좋아요 관련 API", description = "좋아요(like) 관련 API")
 public class LikeController {
 
     private final LikeService likeService;
 
     @GetMapping("/like/post")
+    @Operation(summary = "게시글 좋아요 확인", description = "게시글에 좋아요가 눌려져있는지 확인합니다.")
     public ResponseEntity getPostLike(@RequestParam Long accountId, @RequestParam Long postId) {
         boolean checkedPostLike = likeService.isCheckedPostLike(accountId, postId);
         return ResponseEntity.ok(checkedPostLike);
     }
 
     @GetMapping("/like/comment")
+    @Operation(summary = "댓글 좋아요 확인", description = "댓글에 좋아요가 눌려져있는지 확인합니다.")
     public ResponseEntity getCommentLike(CommentLikeDto commentLikeDto) {
         List<Long> commentLikeList = likeService.getCommentLikeList(commentLikeDto.getAccountId(), commentLikeDto.getPostId());
         return ResponseEntity.ok(commentLikeList);
