e8de9057f27cd15a4f47cd4e8e523f482adf9aa0
남의영 <ny2485@naver.com>
2023-10-20 03:10:33 +0900

    deploy 중지 로직 추가

--- script/jungsuri-deploy.sh
+++ script/jungsuri-deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+#/** 배포시 진행 */
+
 PROJECT_ROOT="/home/ec2-user/jungsuri"
-JAR_FILE="$PROJECT_ROOT/jungsuri-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/build/libs/jungsuri-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -9,9 +11,20 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
+
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/jungsuri-0.0.1-SNAPSHOT.jar
+cp "$JAR_FILE $PROJECT_ROOT/"
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
