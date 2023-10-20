207dd612a4f39e7a41769f3da4953ef74b4d886e
남의영 <ny2485@naver.com>
2023-10-20 16:42:34 +0900

    deploy sh파일 cp 코드 수정

--- script/jungsuri-deploy.sh
+++ script/jungsuri-deploy.sh
@@ -4,7 +4,6 @@
 
 PROJECT_ROOT="/home/ec2-user/jungsuri"
 JAR_FILE="$PROJECT_ROOT/build/libs/jungsuri-0.0.1-SNAPSHOT.jar"
-
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
@@ -24,10 +23,11 @@ fi
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp "$JAR_FILE $PROJECT_ROOT/"
+cp "$JAR_FILE" "$PROJECT_ROOT/"
 
 # "/home/ec2-user/jungsuri/build/libs/jungsuri-0.0.1-SNAPSHOT.jar" "/home/ec2-user/jungsuri"
-
+# nohup java -jar -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+# nohup java -jar -Dspring.profiles.active=prod /home/ec2-user/jungsuri/build/libs/jungsuri-0.0.1-SNAPSHOT.jar
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
