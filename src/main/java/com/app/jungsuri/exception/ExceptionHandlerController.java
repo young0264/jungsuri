2b7772eea2de4c2a8e04405b18e77a840b694f07
남의영 <ny2485@naver.com>
2023-10-26 20:32:30 +0900

    exception controller 추가

--- src/main/java/com/app/jungsuri/exception/ExceptionHandlerController.java
+++ src/main/java/com/app/jungsuri/exception/ExceptionHandlerController.java
@@ -1,2 +1,20 @@
-package com.app.jungsuri.exception;public class ExceptionHandlerController {
+package com.app.jungsuri.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Slf4j
+@Controller
+public class ExceptionHandlerController implements ErrorController {
+
+    @GetMapping("/error")
+    public void handleError(MethodArgumentNotValidException ex) {
+    }
+
 }
