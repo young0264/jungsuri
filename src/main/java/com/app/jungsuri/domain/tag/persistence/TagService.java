7b44a62111ba6125650ba74b96fc0666a0452de8
남의영 <ny2485@naver.com>
2023-10-29 17:04:46 +0900

    account 가입시 태그 목록에 등록 service 로직

--- src/main/java/com/app/jungsuri/domain/tag/persistence/TagService.java
+++ src/main/java/com/app/jungsuri/domain/tag/persistence/TagService.java
@@ -1,13 +1,15 @@
 package com.app.jungsuri.domain.tag.persistence;
 
 import com.app.jungsuri.domain.post.persistence.PostEntity;
+import com.app.jungsuri.domain.tag.persistence.repository.AccountTagReadRepository;
+import com.app.jungsuri.domain.tag.persistence.repository.AccountTagRepository;
 import com.app.jungsuri.domain.tag.persistence.repository.PostTagRepository;
 import com.app.jungsuri.domain.tag.persistence.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.time.LocalDateTime;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +20,7 @@ import java.util.Optional;
 public class TagService {
 
     private final TagRepository tagRepository;
+    private final AccountTagRepository accountTagRepository;
     private final PostTagRepository postTagRepository;
 
     public void deleteTag(Long postId) {
@@ -28,13 +31,21 @@ public class TagService {
         return tagRepository.findByName(name);
     }
 
-    public void createTags(List<String> tagList, PostEntity postEntity) {
+    /** 게시글에 다는 tag 작성*/
+    public void createPostTags(List<String> tagList, PostEntity postEntity) {
         for (String tagName : tagList) {
-            createTag(postEntity, tagName);
+            createPostTag(postEntity, tagName);
         }
     }
 
-    private void createTag(PostEntity postEntity, String tagName) {
+    /** account 생성시 태그 생성*/
+    public void createAccountTags(AccountTag accountTag, Long accountId) {
+        accountTagRepository.save(accountTag);
+    }
+
+    /** mountain 생성시 태그 생성*/
+
+    private void createPostTag(PostEntity postEntity, String tagName) {
         /** 태그가 있으면 used += 1, 태그가 없으면 태그를 생성후 used += 1 */
         Optional<Tag> optionalTag = findByName(tagName);
         optionalTag.ifPresentOrElse(existTag -> {
