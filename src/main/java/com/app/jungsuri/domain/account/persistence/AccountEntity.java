3e7bc0cf31de32632594b76cd91c195944cf7d3a
남의영 <ny2485@naver.com>
2023-10-29 15:59:12 +0900

    account column 매칭

--- src/main/java/com/app/jungsuri/domain/account/persistence/AccountEntity.java
+++ src/main/java/com/app/jungsuri/domain/account/persistence/AccountEntity.java
@@ -27,44 +27,56 @@ public class AccountEntity implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "bio", nullable = true)
     private String bio;
 
     @Column(name = "name", nullable = true)
     private String name;
 
+    @Column(name = "role", nullable = true)
     private String role; //TODO ENUM으로 -> 관리자가 부여해주는 방식
 
-    @Column(unique = true)
+    @Column(name="email", unique = true)
 //    @NotBlank
     private String email;
 
+    @Column(name="email_token")
     private String emailToken;
 
+    @Column(name="email_check_token_generated_at")//
     private LocalDateTime emailCheckTokenGeneratedAt;
 
+    @Column(name="email_verified")
     private boolean emailVerified;
 
+    @Column(name="location")
     private String location; //TODO 대략적인 시 or 구까지.
 
+    @Column(name="occupation")
     private String occupation;//
 
-    @Column(unique = true)
     @NotBlank
+    @Column(name="login_id", unique = true)
     private String loginId;
 
     @NotBlank
+    @Column(name="password")
     private String password;
 
     @Builder.Default
+    @Column(name="profile_image")
     private String profileImage="https://static.nid.naver.com/images/web/user/default.png?type=s160";
 
+    @Column(name="mountain_exp")
     private int mountainExp; //등산한 산 경험치 (높이)
 
     private UserRole userRole;
 
+    @Column(name="joined_at")
     private LocalDateTime joinedAt;
 
     @Builder.Default
+    @Column(name="post_created_checked")
     private boolean postCreatedChecked = false;
 
     @Transient
