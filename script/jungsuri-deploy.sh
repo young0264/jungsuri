c1191d30b8b843484c2ce78b091c5766fc407e3e
남의영 <ny2485@naver.com>
2023-12-25 22:00:42 +0900

    deploy.sh 환경변수error

--- script/jungsuri-deploy.sh
+++ script/jungsuri-deploy.sh
@@ -12,8 +12,8 @@ TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "pid가 왜 안나오니1 : " CURRENT_PID >> $DEPLOY_LOG
-echo "pid가 왜 안나오니2 : " "$CURRENT_PID" >> $DEPLOY_LOG
+#echo "pid가 왜 안나오니1 : " CURRENT_PID >> $DEPLOY_LOG # 이건 문자 current_pid가 나옴
+echo "pid가 왜 안나오니2 : " "$CURRENT_PID" >> $DEPLOY_LOG #실행중인게 없으니 안나오는건 맞음.
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
@@ -25,17 +25,22 @@ fi
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp "$JAR_FILE" "$PROJECT_ROOT/"
+cp "$JAR_FILE" "$PROJECT_ROOT/" #파일 복사 정상적으로 됨
+
 # cp :: "/home/ec2-user/jungsuri/build/libs/jungsuri-0.0.1-SNAPSHOT.jar" "/home/ec2-user/jungsuri"
 # nohup java -jar -Dspring.profiles.active=prod $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 # nohup java -jar -Dspring.profiles.active=prod /home/ec2-user/jungsuri/build/libs/jungsuri-0.0.1-SNAPSHOT.jar
 
 # jar 파일 실행
 echo "$TIME_NOW > $PROJECT_ROOT /jar 파일 실행" >> $DEPLOY_LOG
-nohup java -jar -Dspring.profiles.active=prod "$PROJECT_ROOT"/jungsuri-0.0.1-SNAPSHOT.jar > "$APP_LOG" 2> "$ERROR_LOG &"
+#nohup java -jar -Dspring.profiles.active=prod "$PROJECT_ROOT"/jungsuri-0.0.1-SNAPSHOT.jar > "$APP_LOG" 2 > "$ERROR_LOG &" #& 위치 등 잘못
+nohup java -jar -Dspring.profiles.active=prod "$PROJECT_ROOT"/jungsuri-0.0.1-SNAPSHOT.jar > "$APP_LOG" 2> "$ERROR_LOG" &
+
+
 # nohup java -jar -Dspring.profiles.active=prod /home/ec2-user/jungsuri/jungsuri-0.0.1-SNAPSHOT.jar > /home/ec2-user/jungsuri/application.log 2> /home/ec2-user/jungsuri/error.log &
+#nohup java -jar -Dspring.profiles.active=prod /home/ec2-user/jungsuri/jungsuri-0.0.1-SNAPSHOT.jar > "$APP_LOG" 2 > "$ERROR_LOG &"
 #프로파일 명시
-#CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "current pid 입니다 : " CURRENT_PID >> $DEPLOY_LOG
-echo "달러 current pid 입니다 : $CURRENT_PID" >> $DEPLOY_LOG
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> "$DEPLOY_LOG"
+NEW_PID=$(pgrep -f $JAR_FILE)
+#echo "current pid 입니다 : " CURRENT_PID >> $DEPLOY_LOG # 문자가 나옴
+echo "달러 new pid 입니다 : $NEW_PID" >> $DEPLOY_LOG
+echo "$TIME_NOW > 실행된 프로세스 아이디 $NEW_PID 입니다." >> "$DEPLOY_LOG"
